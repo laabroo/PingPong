6c5197aac3dd4f8d02fb5d5f92195c5c735b12fc
unknown <laabroo@.(none)>
2012-02-01 21:17:20 +0700

    Finished

--- build/preprocessed/hello/ping/MainPing.java
+++ build/preprocessed/hello/ping/MainPing.java
@@ -5,6 +5,7 @@
 package hello.ping;
 
 import java.io.IOException;
+import java.util.Random;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.GameCanvas;
@@ -26,11 +27,24 @@ public class MainPing extends GameCanvas implements Runnable {
     private int ballY = getHeight() / 2;
     private int padX = 10;
     private int padY = getHeight() / 2;
+    private int padScore = 0;
     private int speedBallX = 3;
     private int speedBallY = 1;
-    private static final int speedPadY = 2;
+    private static final int speedPadY = 3;
     private int ballDirection = 1;
+    /**
+     * **************** Computer Player *******************
+     */
+    private Sprite AISprite;
+    private int AIx = getWidth() - 10;
+    private int AIy = getHeight() / 2;
+    private int AIScore = 0;
+    private int speedAIy = 3;
+    private int actX;
 
+    /**
+     * ********************* Implementasi ******************************
+     */
     public MainPing(MIDlet mIDlet) {
         super(false);
         mainMIDlet = mIDlet;
@@ -55,6 +69,10 @@ public class MainPing extends GameCanvas implements Runnable {
         padSprite.defineReferencePixel(1, 10);
         padSprite.setRefPixelPosition(padX, padY);
 
+        AISprite = new Sprite(padImage, 3, 20);
+        AISprite.defineReferencePixel(3, 10);
+        AISprite.setRefPixelPosition(AIx, AIy);
+
         Thread thread = new Thread(this);
         thread.start();
     }
@@ -77,18 +95,34 @@ public class MainPing extends GameCanvas implements Runnable {
         }
     }
 
+    /**
+     * ************************** Function Method *****************************
+     */
     private void createBackground(Graphics graphics) {
         graphics.setColor(0x000000);
         graphics.fillRect(0, 0, getWidth(), getHeight());
     }
 
+    private void createScore(Graphics graphics) {
+        graphics.setColor(0xffffff);
+        graphics.drawString(padScore + " - " + AIScore, getWidth() / 2, 20, Graphics.HCENTER | Graphics.TOP);
+    }
+
     private void updateScreen(Graphics graphics) {
         createBackground(graphics);
+        createScore(graphics);
+
         moveBall();
+        movePaid();
+        moveAI();
+
         ballSprite.setRefPixelPosition(ballX, ballY);
         ballSprite.paint(graphics);
         padSprite.setRefPixelPosition(padX, padY);
         padSprite.paint(graphics);
+        AISprite.setRefPixelPosition(AIx, AIy);
+        AISprite.paint(graphics);
+
         flushGraphics();
     }
 
@@ -120,20 +154,16 @@ public class MainPing extends GameCanvas implements Runnable {
             ballY += speedBallY;
         }
 
-        if (ballDirection == 0 && ballSprite.collidesWith(padSprite, false)) {
-            ballDirection = 1;
-        } else if (ballDirection == 3 && ballSprite.collidesWith(padSprite, false)) {
-            ballDirection = 2;
-        }
-
         if (ballDirection == 0 && ballX < 0) {
             ballDirection = 1;
+            AIScore++;
         } else if (ballDirection == 0 && ballY < 0) {
             ballDirection = 3;
         } else if (ballDirection == 1 && ballY < 0) {
             ballDirection = 2;
         } else if (ballDirection == 1 && ballX > getWidth()) {
             ballDirection = 0;
+            padScore++;
             if (TIME_SLEEP > 5) {
                 TIME_SLEEP--;
             }
@@ -141,6 +171,7 @@ public class MainPing extends GameCanvas implements Runnable {
             ballDirection = 1;
         } else if (ballDirection == 2 && ballX > getWidth()) {
             ballDirection = 3;
+            padScore++;
             if (TIME_SLEEP > 5) {
                 TIME_SLEEP--;
             }
@@ -148,6 +179,39 @@ public class MainPing extends GameCanvas implements Runnable {
             ballDirection = 0;
         } else if (ballDirection == 3 && ballX < 0) {
             ballDirection = 2;
+            AIScore++;
         }
+
+
+        /**
+         * ******* Pad ***********
+         */
+        if (ballDirection == 0 && ballSprite.collidesWith(padSprite, false)) {
+            ballDirection = 1;
+        } else if (ballDirection == 3 && ballSprite.collidesWith(padSprite, false)) {
+            ballDirection = 2;
+        } else if (ballDirection == 1 && ballSprite.collidesWith(AISprite, false)) {
+            ballDirection = 0;
+        } else if (ballDirection == 2 && ballSprite.collidesWith(AISprite, false)) {
+            ballDirection = 3;
+        }
+
+        TIME_SLEEP += AIScore - padScore;
+        if (TIME_SLEEP < 5) {
+            TIME_SLEEP = 5;
+        }
+    }
+
+    private void moveAI() {
+        Random random = new Random();
+        actX = getWidth() / 3 + Math.abs(random.nextInt() % (getWidth() / 8));
+
+        if (ballY < AIy && ballX > actX && AIy > AISprite.getHeight() / 2) {
+            AIy -= speedAIy;
+        }
+        if (ballY > AIy && ballX > actX && AIy < getHeight() - AISprite.getHeight() / 2) {
+            AIy += speedAIy;
+        }
+
     }
 }
